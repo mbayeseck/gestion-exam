0c9df05b0684a1dc685be30d2f9bd0641e4d857f
SECK <mbaye-kebe.seck@atos.net>
2018-02-19 11:16:42 +0100

    first commit

---
+++ src/main/java/com/exam/senegal/gestionexam/GestionExamApplication.java
@@ -0,0 +1,12 @@
+package com.exam.senegal.gestionexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionExamApplication.class, args);
+	}
+}
